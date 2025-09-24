300396d9f001bd2fca752c3fcc6ec07dbf92fa20
Filipe Martins <filipemartinshen@gmail.com>
2025-09-24 11:26:00 -0300

    refactor: Reorganizacao completa da arquitetura do projeto (Service, Repository, DTO) e implementacao do PaisService

--- src/main/java/br/unitins/topicos1/ewine/dto/locationdto/PaisDTOResponse.java
+++ src/main/java/br/unitins/topicos1/ewine/dto/locationdto/PaisDTOResponse.java
@@ -3,13 +3,16 @@ package br.unitins.topicos1.ewine.dto.locationdto;
 import br.unitins.topicos1.ewine.model.locationentities.Pais;
 
 public record PaisDTOResponse(
+    Long id,
     String nome,
     String sigla
 ) {
     public static PaisDTOResponse valueOf (Pais pais) {
         return new PaisDTOResponse(
+            pais.getId(),
             pais.getNome(),
             pais.getSigla()
+            
         );
     }
 }
